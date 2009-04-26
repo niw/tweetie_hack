d09fdfcb507cdcfc3121cb5022b184788699aefe
Yoshimasa Niwa <niw@niw.at>
2009-04-26 16:00:01 +0900

    Enable Growl Notification on the Tweetie for Mac

--- Classes/TweetieHack.h
+++ Classes/TweetieHack.h
@@ -1,10 +1,32 @@
 #import <Cocoa/Cocoa.h>
+#import <Growl/GrowlApplicationBridge.h>
 
-@interface TweetieAppDelegate {
+@interface TwitterUser : NSObject {
+}
+- (NSString *)username;
+- (NSURL *)profileImageURL;
+@end
+
+@interface TwitterStatus : NSObject {
+}
+- (TwitterUser *)fromUser;
+- (NSString *)text;
+- (NSString *)statusID;
+- (BOOL)wasSeen;
+- (void)setWasSeen:(BOOL)seen;
+@end
+
+@interface TwitterAccountStream : NSObject {
+}
+- (NSArray*)statuses;
+- (TwitterStatus *)newestStatus;
+@end
+
+@interface TwitterAccount : NSObject {
 }
 @end
 
 @interface TweetieHack : NSObject {
 }
-+(void) load;
++ (void)load;
 @end
--- Classes/TweetieHack.m
+++ Classes/TweetieHack.m
@@ -1,18 +1,62 @@
 #import "TweetieHack.h"
 #import "MethodSwizzle.h"
 
-@implementation TweetieAppDelegate(TweetieHack)
-- (void)swizzledNewTweetFromAccount:(id)account inReplyToStatus:(id)status defaultText:(id)text replyPointHint:(struct CGPoint *)hint {
-	NSLog(@"newTweetFromAccount: %@ %@ %@", account, status, text);
-	[self swizzledNewTweetFromAccount:account inReplyToStatus:status defaultText:text replyPointHint:hint];
+#define MAX_NO_OF_NOTIFIED_STATUS 5
+
+@implementation TwitterAccount(TweetieHack)
+- (void)swizzled_somethingDidUpdate:(NSNotification*)something {
+	NSLog(@"somethingDidUpdate:%@", something);
+	if([[something name] isEqualToString:@"TwitterStreamDidUpdateNotification"]) {
+		TwitterAccountStream* accountStream = [something object];
+		NSArray *statuses = [accountStream statuses];
+		NSUInteger newestIndex = [statuses indexOfObject:[accountStream newestStatus]];
+		for(NSUInteger i=newestIndex; i<MAX_NO_OF_NOTIFIED_STATUS && i<[statuses count]; i++) {
+			TwitterStatus *status = [statuses objectAtIndex:i];
+			if([status statusID] && ![status wasSeen]) {
+				TwitterUser *fromUser = [status fromUser];
+				NSData *iconData = nil;
+				NSURL *url = [fromUser profileImageURL];
+				if(url) {
+					iconData = [[NSData alloc] initWithContentsOfURL:url];
+				}
+				[GrowlApplicationBridge notifyWithTitle:[fromUser username]
+											description:[status text]
+									   notificationName:[[accountStream className] isEqualToString:@"TwitterRepliesStream"] ? @"Replies" : @"Timeline"
+											   iconData:iconData
+											   priority:0
+											   isSticky:NO
+										   clickContext:nil];
+				[status setWasSeen:YES];
+			}
+		}
+	}
+
+	[self swizzled_somethingDidUpdate:something];
+}
+@end
+
+@implementation NSApplication(TweetieHack)
+- (NSDictionary *)registrationDictionaryForGrowl {
+	NSLog(@"registrationDictionaryForGrowl");
+	return [NSDictionary dictionaryWithObjectsAndKeys:
+			[NSArray arrayWithObjects:@"Timeline", @"Replies", nil], GROWL_NOTIFICATIONS_ALL,
+			[NSArray arrayWithObjects:@"Timeline", @"Replies", nil], GROWL_NOTIFICATIONS_DEFAULT, nil];
 }
 @end
 
 @implementation TweetieHack
-+(void) load {
-	if(MethodSwizzle(NSClassFromString(@"TweetieAppDelegate"),
-					 @selector(newTweetFromAccount:inReplyToStatus:defaultText:replyPointHint:),
-					 @selector(swizzledNewTweetFromAccount:inReplyToStatus:defaultText:replyPointHint:))) {
++ (void)load {
+	NSApplication *app = [NSApplication sharedApplication];
+	NSBundle *appBundle = [NSBundle bundleForClass:[app class]];
+	NSString *growlPath = [[appBundle privateFrameworksPath] stringByAppendingPathComponent:@"Growl.framework"];
+	NSBundle *growlBundle = [NSBundle bundleWithPath:growlPath];
+	if (growlBundle && [growlBundle load]) {
+		objc_msgSend([GrowlApplicationBridge class], @selector(setGrowlDelegate:), app);
+	} else {
+		NSLog(@"Could not load Growl.framework");
+	}
+
+	if(MethodSwizzle(NSClassFromString(@"TwitterAccount"), @selector(somethingDidUpdate:), @selector(swizzled_somethingDidUpdate:))) {
 		NSLog(@"TweetieHack installed");
 	}
 }
